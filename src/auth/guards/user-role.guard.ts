dd54b95263a8ea05eb9d68d4f4f13f9d177f2ab6
Emiliano Merelez <emimerelez@gmail.com>
2026-06-05 20:18:24 -0300

    feat(auth): implement role-protected and unified auth decorators with user-role guard mechanism

---
+++ src/auth/guards/user-role.guard.ts
@@ -0,0 +1,37 @@
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { META_ROLES } from '../decorators/role-protected.decorator';
+import { User } from '../../users/entities/user.entity';
+
+@Injectable()
+export class UserRoleGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const validRoles: string[] = this.reflector.get(
+      META_ROLES,
+      context.getHandler(),
+    );
+
+    if (!validRoles || validRoles.length === 0) return true;
+
+    const req = context.switchToHttp().getRequest();
+    const user = req.user as User;
+
+    if (!user) throw new BadRequestException('User not found');
+
+    for (const role of user.roles) {
+      if (validRoles.includes(role.name)) return true;
+    }
+
+    throw new ForbiddenException(
+      `User ${user.email} needs a valid role: [${validRoles.join(', ')}]`,
+    );
+  }
+}
