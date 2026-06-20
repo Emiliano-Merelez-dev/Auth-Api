6601fabce681a0f6fba1d87a4abfbb5d1366e6ff
Emiliano Merelez <emimerelez@gmail.com>
2026-06-19 22:56:38 -0300

    feat(auth): configure VerificationModule with TypeORM User feature and short-lived JwtModule

--- src/auth/verification/verification.module.ts
+++ src/auth/verification/verification.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { VerificationController } from './verification.controller';
 import { VerificationService } from './verification.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [VerificationController],
   providers: [VerificationService],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: 'llave_secretcodifyVerify_secret',
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  exports: [VerificationService],
 })
 export class VerificationModule {}
