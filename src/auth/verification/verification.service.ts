5f20a23d067b81a21ecf89ecb9dbbf92c2327860
Emiliano Merelez <emimerelez@gmail.com>
2026-06-26 20:40:33 -0300

    feat(auth): implement core token validation and account verification logic in VerificationService

--- src/auth/verification/verification.service.ts
+++ src/auth/verification/verification.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  NotFoundException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class VerificationService {}
+export class VerificationService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  createVerificationToken(email: string) {
+    return this.jwtService.sign({ email }, { expiresIn: '15m' });
+  }
+
+  async verifyAccount(token: string) {
+    let payload: { email: string };
+
+    try {
+      payload = this.jwtService.verify(token);
+    } catch {
+      throw new BadRequestException('Token inválido o expirado');
+    }
+
+    const user = await this.userRepository.findOne({
+      where: { email: payload.email },
+      select: {
+        id: true,
+        isVerified: true,
+      },
+    });
+
+    if (!user) throw new NotFoundException('Usuario no encontrado');
+    if (user.isVerified)
+      throw new BadRequestException('La cuenta ya está verificada');
+
+    await this.userRepository.update(user.id, { isVerified: true });
+
+    return { message: 'Cuenta verificada con éxito' };
+  }
+}
