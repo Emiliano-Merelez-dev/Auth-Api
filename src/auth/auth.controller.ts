796939f9527f972df8f067d00cd4b2cd38b59490
Emiliano Merelez <emimerelez@gmail.com>
2026-07-01 22:25:10 -0300

    feat(auth): protect auth controller endpoints with ThrottlerGuard for rate limiting

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginAuthDto } from './dto/loginAuthDto';
 import { Auth } from './decorators/auth.decorator';
 import { ValidRoles } from 'src/roles/interfaces/valid-roles.interface';
 import { RegisterAuthDto } from './dto/registerAuthDto';
+import { ThrottlerGuard } from '@nestjs/throttler';
 // import { RegisterAuthDto } from './dto/registerAuthDto';
 
 @Controller('auth')
+@UseGuards(ThrottlerGuard)
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
